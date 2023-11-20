fb9698775026c9ff6e960bbef05128336a9831b2
Venkeyekip <139548912+Venkeyekip@users.noreply.github.com>
2023-11-20 18:05:54 +0530

    Add files via upload

--- quick sort.js
+++ quick sort.js
@@ -1,35 +1,37 @@
-function partition(a, l, r) {
-    let pivot = a[r]
+function partition(arr, l, r) {
+    let pivot = arr[r];
 
     let i = l - 1;
 
     for (let j = l; j <= r - 1; j++) {
-        if (a[j] < pivot) {
+        if (arr[j] < pivot) {
             i++;
-            let mj = a[j]
-            a[j] = a[i]
-            a[i] = mj;
+            //Swap arr[i] and arr[j]
+            let temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
     }
 
-    let virus = a[i + 1]
-    a[i + 1] = a[r]
-    a[r] = virus;
+    //Swap (i + 1) with r
+    let temp = arr[i + 1];
+    arr[i + 1] = arr[r]
+    arr[r] = temp;
 
-    return i + 1;
+    return (i + 1);
 }
 
-function name1(a, l, r) {
+
+function quickSort(arr, l, r) {
     if (l < r) {
-        let pi = partition(a, l, r)
-        name1(a, l, pi - 1)
-        name1(a, pi + 1, r)
-        return a;
+        let partitionIndex = partition(arr, l, r);
+        quickSort(arr, l, partitionIndex - 1);
+        quickSort(arr, partitionIndex + 1, r)
     }
+    return arr;
+
 }
 
 
-
-
-let a = [5, 1, 3, 7, 8, 9, 10, 2, 4]
-console.log(name1(a, 0, a.length - 1))
+let arr = [4, 2, 7, 9, 6, 3, 5]
+console.log(quickSort(arr, 0, arr.length - 1))
